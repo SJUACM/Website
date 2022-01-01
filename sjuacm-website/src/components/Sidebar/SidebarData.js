f311d04d95fa66e65e8d95ef9fd81152712f2dcb
faizancodes <faizanahmed80113@gmail.com>
2021-12-31 19:26:00 -0500

    Fixed mobile menu with dropdown for different semesters

---
+++ sjuacm-website/src/components/Sidebar/SidebarData.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
+import * as IoIcons from 'react-icons/io';
+import * as RiIcons from 'react-icons/ri';
+
+export const SidebarData = [
+  {
+    title: 'About',
+    path: '/about',
+    icon: <AiIcons.AiFillHome />,
+  },
+  {
+    title: 'Meetings',
+    path: '/meetings',
+    icon: <IoIcons.IoIosPaper />,
+    iconClosed: <RiIcons.RiArrowDownSFill />,
+    iconOpened: <RiIcons.RiArrowUpSFill />,
+
+    subNav: [
+      {
+        title: 'Fall 2021',
+        path: '/meetings/Fall2021',
+        icon: <IoIcons.IoIosPaper />,
+        cName: 'sub-nav'
+      },
+      {
+        title: 'Spring 2021',
+        path: '/meetings/Spring2021',
+        icon: <IoIcons.IoIosPaper />,
+        cName: 'sub-nav'
+      },
+      {
+        title: 'Fall 2020',
+        path: '/meetings/Fall2020',
+        icon: <IoIcons.IoIosPaper />
+      }
+    ]
+  },
+  {
+    title: 'E-Board',
+    path: '/eboard',
+    icon: <AiIcons.AiFillHome />,
+  },
+  {
+    title: 'Resources',
+    path: '/resources',
+    icon: <AiIcons.AiFillHome />,
+  }
+];
--- sjuacm-website/src/components/Sidebar/index.js
+++ sjuacm-website/src/components/Sidebar/index.js
@@ -1,56 +1,35 @@
 import React, { useState } from 'react';
 import { SidebarContainer, Icon, CloseIcon, SidebarWrapper, SidebarMenu, SidebarLink, SidebarSubLink, SidebarSubLinkContainer, SideBtnWrap, SidebarRoute } from './SidebarElements'
 import { BrowserRouter as Router, Link, Route} from 'react-router-dom';
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
+import { SidebarData } from './SidebarData';
+import SubMenu from './SubMenu';
+import { IconContext } from 'react-icons/lib';
+import styled from 'styled-components';
+
+
+
+const SidebarWrap = styled.div`
+  width: 100%;
+  padding-left: 5rem;
+  
+`;
 
 const Sidebar = ({ isOpen, toggle }) => {
     
     return (
         <>
-        <SidebarContainer isOpen={isOpen} onClick={toggle}>
+        <SidebarContainer isOpen={isOpen}>
             <Icon onClick={toggle}>
                 <CloseIcon />
             </Icon>
 
-            <SidebarWrapper>
-
-                <SidebarMenu>
-
-                    <SidebarLink>
-                        <Link to='/about' style={{textDecoration: 'none', color: 'white'}}>About</Link>
-                    </SidebarLink>
-                    
-                    <SidebarLink>
-                        <Link to='/meetings' style={{textDecoration: 'none', color: 'white', marginTop: '30px'}}>Meetings</Link>
-                    </SidebarLink>
-    
-                    <SidebarSubLink>
-                        <Link to='/meetings/Fall2021' style={{textDecoration: 'none', color: 'white', marginTop: '70px', paddingBottom: '10px'}}>- Fall 2021</Link>
-                    </SidebarSubLink>
-
-                    <SidebarSubLink>
-                        <Link to='/meetings/Spring2021' style={{textDecoration: 'none', color: 'white', marginTop: '80px', paddingBottom: '20px'}}>- Spring 2021</Link>
-                    </SidebarSubLink>
-
-                    <SidebarSubLink>
-                        <Link to='/meetings/Fall2020' style={{textDecoration: 'none', color: 'white', marginTop: '80px',  paddingBottom: '20px'}}>- Fall 2020</Link>
-                    </SidebarSubLink>
-            
-                    
-                    <SidebarLink>
-                        <Link to='/eboard' style={{textDecoration: 'none', color: 'white', marginTop: '250px'}}>E-Board</Link>
-                    </SidebarLink>
-
-                    <SidebarLink>
-                        <Link to='/resources' style={{textDecoration: 'none', color: 'white'}}>Resources</Link>
-                    </SidebarLink>
-
-                </SidebarMenu>
-
-                <SideBtnWrap style={{paddingTop: '150px'}}>
-                    <SidebarRoute to="/" onClick={() => window.location.href='https://forms.gle/CGcyq93XaLUfMAkz9'}>Join our Mailing List</SidebarRoute>
-                </SideBtnWrap>
-
-            </SidebarWrapper>
+            <SidebarWrap>
+                {SidebarData.map((item, index) => {
+                return <SubMenu item={item} key={index}/>;
+                })}
+            </SidebarWrap>
 
             
 

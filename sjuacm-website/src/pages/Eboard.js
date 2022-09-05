f11fbd9acda7200b1c3ed33efdf0fd7446ebd18d
Jake Enea <91490989+jakeenea51@users.noreply.github.com>
2022-09-05 12:45:50 -0400

    Updated eboard javascript

--- sjuacm-website/src/pages/Eboard.js
+++ sjuacm-website/src/pages/Eboard.js
@@ -19,13 +19,12 @@ const Eboard = () => {
         <>
             <Sidebar isOpen={isOpen} toggle={toggle}/>
             <Navbar toggle={toggle} about_border_bottom={false} meeting_border_bottom={false} eboard_border_bottom={true} resources_border_bottom={false}/>
-            <EboardSection {...Faizan} />
-            <EboardSection {...Tom} />
-            <EboardSection {...Teuta} />
             <EboardSection {...Raymond} />
+            <EboardSection {...Teuta} />
             <EboardSection {...Jake} />
-            <EboardSection {...Ava} />
-            <EboardSection {...Jade} />
+            <EboardSection {...David} />
+            <EboardSection {...Ignacio} />
+            <EboardSection {...Katarina} />
 
             <PrevEboardSection />
 

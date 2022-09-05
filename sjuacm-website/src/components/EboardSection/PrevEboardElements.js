7f1690ea953cdadd5ae4c4746e70d52f272c7769
Jake Enea <91490989+jakeenea51@users.noreply.github.com>
2022-09-05 13:44:13 -0400

    Eboard page sizing fix

--- sjuacm-website/src/components/EboardSection/PrevEboardElements.js
+++ sjuacm-website/src/components/EboardSection/PrevEboardElements.js
@@ -8,13 +8,14 @@ export const CardContainer = styled.div`
     justify-content: center;
     align-items: center;
     padding: 0 30px;
-    height: 1350px;
+    height: 1000px;
     position: relative;
     z-index: 1;
-    margin-bottom: -150px;
+    margin-bottom: -100px;
     flexWrap: wrap;
     margin-top: -180px;
     padding-top: 1200px;
+    padding-bottom: -300px;
 
     @media screen and (max-width: 1060px) {
         height: 1500px;

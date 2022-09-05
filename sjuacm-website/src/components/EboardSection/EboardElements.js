00d0f4750b6c75edb56b0c6a1ecd2816b9cd7acd
Jake Enea <91490989+jakeenea51@users.noreply.github.com>
2022-09-05 18:51:22 -0400

    Mobile sizing fix

--- sjuacm-website/src/components/EboardSection/EboardElements.js
+++ sjuacm-website/src/components/EboardSection/EboardElements.js
@@ -26,7 +26,7 @@ export const EboardContainer = styled.div`
 export const InfoWrapper = styled.div`
     display: grid;
     z-index: 1;
-    height: 900px;
+    height: 1000px;
     width: 100%;
     max-width: 1100px;
     margin-right: auto;
--- sjuacm-website/src/components/EboardSection/PrevEboardElements.js
+++ sjuacm-website/src/components/EboardSection/PrevEboardElements.js
@@ -17,7 +17,7 @@ export const CardContainer = styled.div`
     padding-top: 500px;
 
     @media screen and (max-width: 1060px) {
-        height: 2000px;
+        height: 1500px;
         text-align: center;
     }
 

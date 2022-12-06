16886420cecda5b3957cf5d17ec736c5087bd9bc
Jake Enea <jakemenea@gmail.com>
2022-12-05 22:56:14 -0500

    Updated upcoming meetings

--- sjuacm-website/src/components/UpcomingMeetings/data.js
+++ sjuacm-website/src/components/UpcomingMeetings/data.js
@@ -1,9 +1,9 @@
 export const upcomingMeetings = [
-    {
-        date: 'Monday, December 5, 2022',
-        time: '1:50pm - 3:00pm',
-        location: 'Cyber Lab (St. Augustine Hall, 2nd Floor)',
-        name : 'Faculty Research Opportunities and Service Event',
-        description : "Volunteer in helping campus ministry put together Christmas candy bags and hear from faculty members about the many research opportunities they offer and how you can get involved. Research is a great way to learn outside of the classroom and strengthen your resume while doing so!",
-    }
+    // {
+    //     date: 'Monday, December 5, 2022',
+    //     time: '1:50pm - 3:00pm',
+    //     location: 'Cyber Lab (St. Augustine Hall, 2nd Floor)',
+    //     name : 'Faculty Research Opportunities and Service Event',
+    //     description : "Volunteer in helping campus ministry put together Christmas candy bags and hear from faculty members about the many research opportunities they offer and how you can get involved. Research is a great way to learn outside of the classroom and strengthen your resume while doing so!",
+    // }
 ]

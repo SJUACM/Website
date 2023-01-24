2699d36c398e77dfd20856b76704f8646a25d565
Jake Enea <jakemenea@gmail.com>
2023-01-24 13:01:22 -0500

    Updated upcoming meetings

--- sjuacm-website/src/components/UpcomingMeetings/data.js
+++ sjuacm-website/src/components/UpcomingMeetings/data.js
@@ -6,4 +6,26 @@ export const upcomingMeetings = [
     //     name : 'Faculty Research Opportunities and Service Event',
     //     description : "Volunteer in helping campus ministry put together Christmas candy bags and hear from faculty members about the many research opportunities they offer and how you can get involved. Research is a great way to learn outside of the classroom and strengthen your resume while doing so!",
     // }
+    {
+        date: 'Thursday, January 26, 2023',
+        time: '1:50pm - 3:00pm',
+        location: 'Cyber Lab (St. Augustine Hall, 2nd Floor)',
+        name : 'Spring 2023 Kickoff | Basic Linux Commands',
+        description : "Hear about our upcoming meetings for Spring 2023, followed by an introductory lab on basic Linux commands!",
+    },
+    {
+        date: 'Monday, January 30, 2023',
+        time: '2:00pm - 3:00pm',
+        location: 'Taffner Field House',
+        name : 'Spring 2023 Activities Fair',
+        description : "Come by our table at the Spring 2023 Activities Fair to talk to our e-board about the club and ask us any questions you may have!",
+    }
+    // {
+    //     date: 'Thursday, February 9, 2023',
+    //     time: '1:50pm - 3:00pm',
+    //     location: 'Cyber Lab (St. Augustine Hall, 2nd Floor)',
+    //     name : 'Phishing Lab | Cyber Competition Pitch',
+    //     description : "Come by our table at the Spring 2023 Activities Fair to talk to our e-board about the club and ask us any questions you may have!", 
+    // }
+
 ]

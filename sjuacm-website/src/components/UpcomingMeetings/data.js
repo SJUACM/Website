54fb022c33940ceff2edbfddf772d6934d28bf6e
jake.enea51 <jakemenea@gmail.com>
2023-09-12 21:21:03 -0400

    Updated upcoming meetings

--- sjuacm-website/src/components/UpcomingMeetings/data.js
+++ sjuacm-website/src/components/UpcomingMeetings/data.js
@@ -8,22 +8,31 @@ export const upcomingMeetings = [
     //     link: ""
     // },
 
-    {
-        date: 'Thursday, September 7, 2023',
-        time: '12:00pm - 3:00pm',
-        location: 'Great Lawn',
-        name : 'Fall 2023 Activities Fair',
-        description : "Stop by the SJU ACM table to talk to members of our e-board, learn about our club, and get a sneak peak at what we have planned for this semester!",
-        link: ""
-    },
-
 
     {
         date: 'Thursday, September 14, 2023',
         time: '1:50pm - 3:00pm',
         location: 'Cyber Lab (St. Augustine Hall, 2nd Floor)',
         name : 'Fall 2023 Kickoff',
-        description : "Join us as we kickoff the Fall semester with e-board introductions, an overview of SJU ACM, and an outline of some of the meetings we have planned!",
+        description : "Join us as we kickoff the Fall semester with e-board introductions, an overview of SJU ACM, an outline of some of the meetings we have planned, and a demonstration how to perform a denial of service attack (which you will get to try out for yourselves)!",
+        link: ""
+    },
+
+    {
+        date: 'Thursday, September 28, 2023',
+        time: '12:00pm - 4:00pm',
+        location: 'Taffner Field House',
+        name : 'Fall Career & Internship Expo 2023',
+        description : "Looking for an internship or full-time position for Summer 2024? Attend this expo to get the chance to meet and speak with over 100 different employers! Make sure to dress in business casual and bring several copies of your resume!",
+        link: "https://stjohns.joinhandshake.com/stu/career_fairs/40708?ref=events-search"
+    },
+
+    {
+        date: 'Thursday, October 5, 2023',
+        time: '1:50pm - 3:00pm',
+        location: 'Cyber Lab (St. Augustine Hall, 2nd Floor)',
+        name : 'Digital Forensics Investigation Lab',
+        description : "In this workshop, you will get the chance to perform a digital forensics investigation on a USB drive and uncover all of the secrets that lie within it...",
         link: ""
     },
 
